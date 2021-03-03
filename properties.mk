b7183a01bbe0300b247f6c92fc0dd8a99ba3ef29
Nick C <pokemonnekitgod@gmail.com>
2021-03-03 09:24:31 +0100

    platina: switch to march redfin fp

--- properties.mk
+++ properties.mk
@@ -12,3 +12,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.ubwc=0 \
     persist.vendor.camera.stats.test=0 \
     vendor.camera.aux.packagelist=com.android.camera
+
+# Device mask
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys \
+    ro.product.brand=google \
+    ro.product.device=redfin \
+    ro.product.manufacturer=Google \
+    ro.product.model=Pixel 4a (5G) \
+    ro.product.name=redfin \
